6fc2d7d80cadb14df11a6be79e419289b7573e9b
lwainright <33648073+lwainright@users.noreply.github.com>
2026-03-15 03:40:37 -0400

    Add loadDemoData function to fetch demo data

--- utils/demo.js
+++ utils/demo.js
@@ -1,1 +1,8 @@
+export const loadDemoData = async () => {
+  const pst = await fetch("demo/pst.json").then(r => r.json());
+  const cit = await fetch("demo/cit.json").then(r => r.json());
+  const resources = await fetch("demo/resources.json").then(r => r.json());
+
+  return { pst, cit, resources };
+};
 
