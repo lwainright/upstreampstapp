49aa35d8ef9a32aa7c11f0c921bc833158a1b062
lwainright <33648073+lwainright@users.noreply.github.com>
2026-03-15 03:50:38 -0400

    Add CIT_TEMPLATE export with initial structure

--- data/citTemplate.js
+++ data/citTemplate.js
@@ -1,1 +1,15 @@
+export const CIT_TEMPLATE = {
+  id: "",
+  name: "",
+  coordinator: {
+    name: "",
+    role: "",
+    contact: ""
+  },
+  activationSteps: [],
+  specialties: [],
+  roster: [],
+  resources: [],
+  onCallScheduleUrl: ""
+};
 
