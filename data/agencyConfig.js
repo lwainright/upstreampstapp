ab41bfde3829564a1cb7c774c07bfc30ed8b6c2a
lwainright <33648073+lwainright@users.noreply.github.com>
2026-03-15 03:51:14 -0400

    Add DEFAULT_AGENCY_CONFIG to agencyConfig.js

--- data/agencyConfig.js
+++ data/agencyConfig.js
@@ -1,1 +1,12 @@
+export const DEFAULT_AGENCY_CONFIG = {
+  branding: {
+    logoUrl: "/icons/icon-512.png",
+    primaryColor: "#38bdf8",
+    headerImageUrl: ""
+  },
+  pstRoster: [],
+  citTeam: null,
+  resources: [],
+  accessCodes: []
+};
 
