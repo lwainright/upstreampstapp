0339199a1496b5c93b0fe3038a4dd9d4958fa4c5
lwainright <33648073+lwainright@users.noreply.github.com>
2026-04-02 22:54:10 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7036,7 +7036,6 @@ if(loading){
     <div style={{position:"relative",width:"100vw",overflowX:"hidden",overflowY:"hidden"}}>
       {showSplash&&<SplashScreen logoSrc={LOGO_FULL_SRC}...
 ```
-
 **So it looks like this in the file:**
 ```
 ...last useEffect or function above...
