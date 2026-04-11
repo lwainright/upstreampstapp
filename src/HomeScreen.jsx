154e988786e2366ba98b61405c0e79407f9cb0e8
lwainright <33648073+lwainright@users.noreply.github.com>
2026-04-10 22:07:18 -0400

    Refactor HomeScreen component and remove Vite config

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -1,69 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import React from 'react';
+import { Screen } from './ui.jsx';
 
-export default defineConfig({
-plugins: [
-react(),
-VitePWA({
-registerType: 'autoUpdate',
-includeAssets: ['icons/*.png', 'offline.html'],
-manifest: {
-name: 'Upstream Peer Support',
-short_name: 'Upstream',
-description: 'First Responder Peer Support & Wellness',
-theme_color: '#060e1b',
-background_color: '#060e1b',
-display: 'standalone',
-orientation: 'portrait',
-scope: '/',
-start_url: '/',
-icons: [
-{ src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-{ src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
-{ src: '/icons/maskable-icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
-]
-},
-workbox: {
-globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-cleanupOutdatedCaches: true,
-skipWaiting: true,
-clientsClaim: true,
-navigateFallback: null,
-runtimeCaching: [
-{
-urlPattern: /^https:\/\/ipapi\.co/,
-handler: 'StaleWhileRevalidate',
-options: {
-cacheName: 'ip-detection',
-expiration: { maxAgeSeconds: 60 * 60 * 24 * 30 }
+export default function HomeScreen({ agency, logoSrc }) {
+return (
+<Screen headerProps={{ agencyName: agency?.name, logoSrc }}>
+<div style={{ color: '#dde8f4', padding: 16 }}>
+Home loaded.
+</div>
+</Screen>
+);
 }
-},
-{
-urlPattern: /^https:\/\/fonts\.googleapis\.com/,
-handler: 'StaleWhileRevalidate',
-options: { cacheName: 'google-fonts-stylesheets' }
-},
-{
-urlPattern: /^https:\/\/fonts\.gstatic\.com/,
-handler: 'CacheFirst',
-options: {
-cacheName: 'google-fonts-webfonts',
-expiration: { maxAgeSeconds: 60 * 60 * 24 * 365 }
-}
-}
-]
-}
-})
-],
-build: {
-outDir: 'dist',
-sourcemap: false,
-rollupOptions: {
-output: {
-manualChunks: {
-react: ['react', 'react-dom']
-}
-}
-}
-}
-})
