ef2903d3b86494033bf30e0b6610b0be4f430841
lwainright <33648073+lwainright@users.noreply.github.com>
2026-04-16 15:28:43 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ['react', 'react-dom']
-        }
-      }
-    }
-  }
+        entryFileNames: `assets/[name]-[hash]-v3.js`,
+        chunkFileNames: `assets/[name]-[hash]-v3.js`,
+        assetFileNames: `assets/[name]-[hash]-v3.[ext]`,
+      },
+    },
+  },
 })
